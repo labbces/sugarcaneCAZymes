eb35fcfa37b7ae72f02bf5b8f0277fb31ebf9970
Diego M Riano-Pachon <diriano@gmail.com>
2025-09-03 15:35:08 -0300

    compare subfamilies of CAZYDB and eCAMI

--- scripts/compareCazyDB_dbCAN.py
+++ scripts/compareCazyDB_dbCAN.py
@@ -1,22 +1,19 @@
-import glob
 import gzip
 
 dbcan_results = {}
 
-# Use the provided path pattern
-for filename in glob.glob("/data/dmrp/cazy/cazy_proteins_CAZymes_*/overview.tsv"):
-    with open(filename, "r") as f:
-        for line in f:
-            parts = line.strip().split('\t')
-            if len(parts) < 2:
-                continue
-            # Check if last but one column equals 3
-            if parts[-2] == "3":
-                key = parts[0]
-                value = parts[-1]
-                dbcan_results[key] = value
-
-
+with gzip.open("cazy_proteins_dbcan.tsv.gz", "rt") as f:
+    for line in f:
+        if line.startswith("Gene ID"):
+            continue
+        parts = line.strip().split('\t')
+        if len(parts) < 2:
+            continue
+        # Check if last but one column equals 3
+        if parts[-2] == "3":
+            SeqID = parts[0]
+            family = parts[-1]
+            dbcan_results[SeqID] = family
 
 cazy_classification = {}
 
@@ -26,10 +23,27 @@ with gzip.open("cazy_data_20250707.txt.gz", "rt") as f:
         if len(parts) != 5:
             continue
         if parts[-1] == "ncbi":
-            key = parts[-2]
-            value = parts[0]
-            cazy_classification[key] = value
+            seqID = parts[-2]
+            family = parts[0]            
+            if seqID not in cazy_classification:
+                cazy_classification[seqID] = {}
+            cazy_classification[seqID][family] = True
 
-for key in dbcan_results:
-    if key in cazy_classification:
-        print(f"{key}\t{dbcan_results[key]}\t{cazy_classification[key]}")
+
+combo_dict = {}
+
+with open("dbcan_vs_cazy_families_perseq.tsv", "a") as out_f:
+    for key in dbcan_results:
+        if key in cazy_classification:
+            comb_key = (dbcan_results[key], cazy_classification[key])
+            combo_dict.setdefault(dbcan_results[key], set()).update(cazy_classification[key].keys())
+            out_f.write(f"{key}\t{dbcan_results[key]}\t{','.join(cazy_classification[key].keys())}\n")
+        # print(f"{key}\t{dbcan_results[key]}\t{', '.join(cazy_classification[key].keys())}")
+
+
+combo_dict = dict(sorted(combo_dict.items()))
+with open("dbcan_family_vs_cazy_families.tsv", "w") as summary_f:
+    for dbcan_family, cazy_families in combo_dict.items():
+        cazy_families_str = ",".join(sorted(cazy_families))
+        summary_f.write(f"{dbcan_family}\t{cazy_families_str}\n")
+        # print(f"{dbcan_family}\t{cazy_families_str}")
